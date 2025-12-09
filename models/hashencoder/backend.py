64d0bfb2b965e32b7b9c6d2433c28bf1099d31ab
__stdcall <hanghang0702@hotmail.com>
2025-12-09 19:12:19 +0800

    change c++ version to suppress compiler error

--- models/hashencoder/backend.py
+++ models/hashencoder/backend.py
@@ -10,7 +10,7 @@ _src_path = os.path.dirname(os.path.abspath(__file__))
 _backend = load(name='_hash_encoder',
                 extra_cflags=['-O3', '-std=c++14'],
                 extra_cuda_cflags=[
-                    '-O3', '-std=c++14', '-allow-unsupported-compiler',
+                    '-O3', '-std=c++17', '-allow-unsupported-compiler',
                     '-U__CUDA_NO_HALF_OPERATORS__', '-U__CUDA_NO_HALF_CONVERSIONS__', '-U__CUDA_NO_HALF2_OPERATORS__',
                 ],
                 sources=[os.path.join(_src_path, 'src', f) for f in [
