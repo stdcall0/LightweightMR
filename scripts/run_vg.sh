edb9128360607c2b4754bb315d6926870c4194d4
__stdcall <hanghang0702@hotmail.com>
2025-12-11 20:14:09 +0800

    fix

--- scripts/run_vg.sh
+++ scripts/run_vg.sh
@@ -1,8 +1,9 @@
-GPU_IDX=1
+GPU_IDX=0
 SDF_SUBDATADIR=SDF
 SDF_CHECKPOINT=ckpt_020000.pth
 SUBDATADIR=VG
 VG_CHECKPOINT=ckpt_008000.pth
+OPT_PERF=${OPT_PERF:-on}
 
 DATA_DIR=./example/data/
 EXP_DIR=./example/exp/
@@ -10,31 +11,17 @@ EXP_DIR=./example/exp/
 run_with_timing() {
     local desc="$1"; shift
     local start=$(date +%s)
-    local tmp_log=$(mktemp)
-    /usr/bin/env time -f "%e %P %M" "$@" 2> "$tmp_log"
+    /usr/bin/env time -f "[time] %e s elapsed | CPU %P | MaxRSS %M KB" "$@"
     exit_code=$?
     local end=$(date +%s)
-    read elapsed cpu rss_kb < "$tmp_log"
-    rm -f "$tmp_log"
-    rss_mib=$(python - <<PY
-import math
-try:
-    v = float("${rss_kb:-0}")
-    print(f"{v/1024:.2f}")
-except Exception:
-    print("0.00")
-PY
-)
-    echo "[time] ${elapsed} s elapsed | CPU ${cpu} | MaxRSS ${rss_mib} MiB (${rss_kb} KB)"
     echo "[wall] ${desc} took $((end-start)) s (exit $exit_code)"
     return $exit_code
 }
 
-for SCAN in "47984" "44234" "354371"
-do
+for SCAN in "47984" "44234" "354371"; do
     CONF="./confs/vg.conf"
     run_with_timing "vg-train $SCAN" python run_vg.py --conf $CONF --mode train --sdf_subdatadir $SDF_SUBDATADIR --sdf_checkpoint_name $SDF_CHECKPOINT \
-    --datadir $DATA_DIR --expdir $EXP_DIR --dataname $SCAN --subdatadir $SUBDATADIR --gpu $GPU_IDX
+    --datadir $DATA_DIR --expdir $EXP_DIR --dataname $SCAN --subdatadir $SUBDATADIR --gpu $GPU_IDX --opt_perf $OPT_PERF
     run_with_timing "vg-validate $SCAN" python run_vg.py --conf $CONF --mode validate_mesh_delaunay --sdf_subdatadir $SDF_SUBDATADIR --sdf_checkpoint_name $SDF_CHECKPOINT \
-    --datadir $DATA_DIR --expdir $EXP_DIR --dataname $SCAN --subdatadir $SUBDATADIR --gpu $GPU_IDX --checkpoint_name $VG_CHECKPOINT
+    --datadir $DATA_DIR --expdir $EXP_DIR --dataname $SCAN --subdatadir $SUBDATADIR --gpu $GPU_IDX --checkpoint_name $VG_CHECKPOINT --opt_perf $OPT_PERF
 done
